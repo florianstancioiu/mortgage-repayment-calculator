0e50ae852e1496dbe377e5c13ecac6179cc92ef2
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2025-05-23 14:56:48 +0300

    Worked on the mobile RadioInput component

--- src/components/RadioInput/RadioInput.vue
+++ src/components/RadioInput/RadioInput.vue
@@ -1,14 +1,29 @@
 <script setup lang="ts">
-defineProps<{ title: string; prefix?: string; suffix?: string }>();
+const { title, name, values } = defineProps<{
+  title: string;
+  name: string;
+  values: { id: number; title: string }[];
+}>();
 </script>
 
 <template>
-  <div>
-    <p>{{ title }}</p>
-    <div>
-      <p v-if="prefix">{{ prefix }}</p>
-      <input type="text" />
-      <p v-if="suffix">{{ suffix }}</p>
+  <div class="mb-[1.875rem]">
+    <label class="text-secondary mb-4 inline-block">{{ title }}</label>
+    <div
+      v-for="val in values"
+      class="mb-[0.625rem] pl-[1.125rem] w-full flex items-center gap-x-[1.25rem] border-secondary border-[0.063rem] relative rounded-[0.25rem] h-[3.125rem]"
+    >
+      <input
+        :id="val.title.toLowerCase().replace(' ', '_')"
+        :name="name"
+        class=""
+        type="radio"
+      />
+      <label
+        class="text-primary"
+        :for="val.title.toLowerCase().replace(' ', '_')"
+        >{{ val.title }}</label
+      >
     </div>
   </div>
 </template>
